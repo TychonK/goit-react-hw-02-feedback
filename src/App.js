91d9df23ee480e5fce85becd092de286a0e02d0c
Tychon <tisha.kruglyak@gmail.com>
2021-10-08 11:31:03 +0200

    basic buttons added with stats

--- src/App.js
+++ src/App.js
@@ -7,9 +7,38 @@ class App extends Component {
     neutral: 0,
     bad: 0
   }
-  return(
-    
-  )
+
+  handleIncrementGood = () => {
+    this.setState({
+      good: this.state.good + 1,
+    })
+  }
+  handleIncrementNeutral = () => {
+    this.setState({
+      neutral: this.state.neutral + 1,
+    })
+  }
+  handleIncrementBad = () => {
+    this.setState({
+      bad: this.state.bad + 1,
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Please leave feedback</h1>
+        <button onClick={this.handleIncrementGood}>Good</button>
+        <button onClick={this.handleIncrementNeutral}>Neutral</button>
+        <button onClick={this.handleIncrementBad}>Bad</button>
+        <h2>Statistics</h2>
+        <span>Good: { this.state.good } </span>
+        <span>Neutral: { this.state.neutral } </span>
+        <span>Bad: {this.state.bad} </span>
+        <span>Total: </span>
+      </div>
+    )
+  }
 }
 
 export default App;
